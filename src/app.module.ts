b8a318fa5240695ca3bbc652c850208361b9bdbc
Kirill Dyachkov <dj_pro98@mail.ru>
2024-10-02 14:43:08 +0600

    Модуль приложения

---
+++ src/app.module.ts
@@ -0,0 +1,34 @@
+import { Module } from '@nestjs/common';
+import { AppUpdate} from './app.update';
+import { TasksService } from './tasks.service';
+import { TelegrafModule } from 'nestjs-telegraf';
+import * as LocalSession from 'telegraf-session-local';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+import { TaskEntity } from './Task.entity';
+
+const sessions = new LocalSession({database: 'session_db.json'})
+
+@Module({
+  imports: [
+    TelegrafModule.forRoot({
+      middlewares: [sessions.middleware()],
+      token: process.env.TOKEN_BOT
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      database: 'tasks_tg_bot',
+      username: 'postgres',
+      password: '123865',
+      entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+      migrations: [join(__dirname, '**', '*.migration.{ts,js}')],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([TaskEntity])
+    
+  ],
+  providers: [TasksService, AppUpdate],
+})
+export class AppModule {}
